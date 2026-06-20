717091e96b0c9dc98f4dc50e1eac0d00f93719c9
mahdi nobahar <nobahar.mahdi@yahoo.com>
2026-06-20 20:47:37 +0330

    first commit

---
+++ p1.php
@@ -0,0 +1,11 @@
+<?php
+$var = 1;
+$Var = 2;
+$array = [1, 2, 3];
+
+echo $var . '<br>';
+echo $Var . '<br>';
+
+var_dump($array);
+echo '<br>';
+print_r($array);
---
+++ p2.php
@@ -0,0 +1,4 @@
+<?php
+$int = 23;
+echo 23, "<br>";
+$string = "Mahdi <br>";
